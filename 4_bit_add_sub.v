bb887cf0dcfdee83fd17da4ed708b6d970cd5682
Satish Kumar <satishkumar250303@gmail.com>
2026-02-15 12:38:03 +0530

    Update 4_bit_add_sub.v

--- 4_bit_add_sub.v
+++ 4_bit_add_sub.v
@@ -5,17 +5,10 @@ module bits(sum,cout,a,b,m);
     output cout;
     wire t1,t2,t3;
 
-        
         full_adder s0(sum[0],t1,a[0],(b[0]^ m),m);
-        //carry c1(t1,a[0],(b[0]^ m),m);
         full_adder s1(sum[1],t2,a[1],(b[1]^ m),t1);
-        //carry c2(t2,a[1],(b[1]^ m),t1);
         full_adder s2(sum[2],t3,a[2],(b[2]^ m),t2);
-        //carry c3(t1,a[0],(b[2]^ m),t2);
         full_adder s3(sum[3],cout,a[3],(b[3]^m),t3);
-        //carry c4(cout,a[3],(b[3]^m),t3);
-    
-
 endmodule
 
 module full_adder(sum,cout,a,b,cin);
