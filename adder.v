d03ac0026e8206fe27f651139a21d084686b908a
Satish Kumar <satishkumar250303@gmail.com>
2026-02-17 17:57:19 +0530

    Update adder.v

--- adder.v
+++ adder.v
@@ -2,7 +2,7 @@ module full_adder(sum,cout,a,b,cin);
     input a,b,cin;
     //wire t1;
     output sum, cout;
-
+    //assign {cout,sum} = a+b+c;
     sum s1(t1,a,b);
     sum s2(sum, t1,cin);
     carry c(cout, a,b,cin);
