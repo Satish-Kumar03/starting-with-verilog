5132e865f695ac8cda6be8e04c40a10bff822923
Satish Kumar <satishkumar250303@gmail.com>
2026-03-02 21:46:32 +0530

    Refactor XOR generation to use my_xor module

--- generate.v
+++ generate.v
@@ -2,10 +2,16 @@ module gen #(parameter N=16)(input [N-1:0] x,y, output [N-1:0] out);
   genvar i;
 
   generate for(i=0; i<=N-1;i=i+1)
-    begin xorli
-      xor xg(out[i],x[i],y[i]);
+    begin : xorli
+      //xor xg(out[i],x[i],y[i]);
+      my_xor g1(.out(out[i]), .x(x[i]), .y(y[i]));
     end
   endgenerate
 
 endmodule
-    
+
+module my_xor(input x,y, output out);
+  assign out = x^y;
+endmodule
+
+  
