190aefe7730a78a9b6bc6fd584462f7022003978
Satish Kumar <satishkumar250303@gmail.com>
2026-03-06 19:14:05 +0530

    Add comments and endmodule to tflipflop.v

--- tflipflop.v
+++ tflipflop.v
@@ -23,3 +23,23 @@ module Tflipflop (
         end
     end
 endmodule
+
+
+// primitive Tflipflop(q, t,clk,rst);
+//     input rst,clk,t;
+//     output reg q;
+//     initial q=0;
+//     table 
+//         // clr.    clk.     t.   :    pre.     next
+//         1           ?       ?    :    ?   :   0;
+//         (10)        ?       ?    :    ?   :   -;   // falling edge of rst
+
+//         0           (01)    0     :   ?   :   -;   // hold state
+
+//         0           (01)    1     :   0   :   1;
+//         0           (01)    1     :   1   :   0;  /// toggle state 
+        
+//         0           (10)    ?     :   ?   :   -;  // falling edge of clk
+//     endtable         
+
+// endprimitive
