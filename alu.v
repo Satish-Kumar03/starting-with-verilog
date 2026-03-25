a4d71aa4f0d075d127e9cbe704d580152a22bba8
Satish Kumar <satishkumar250303@gmail.com>
2026-03-25 21:33:54 +0530

    Add comments to alu.v for clarity

--- alu.v
+++ alu.v
@@ -3,10 +3,10 @@ module ALU(x,y,z,sign,zero,carry,overflow,parity);
     output [15:0] z;
     output sign,zero,carry,parity,overflow;
     
-    assign sign = z[15];
+    assign sign = z[15];       // most significant bit in signed numbers
     assign zero = ~|z;
-    assign parity = ~^z;
+    assign parity = ~^z;       // checks number of 1's 
     assign overflow = (x[15]&y[15]&~z[15]) | (~x[15]&~y[15]&z[15]);
 
-    assign {carry,z} = x+y;
+    assign {carry,z} = x+y;     // concatenation operation 
 endmodule
