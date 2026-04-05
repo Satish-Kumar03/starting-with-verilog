8c55627531860b168ef07d8bd3262f3389cfd28a
Satish Kumar <satishkumar250303@gmail.com>
2026-04-05 10:03:51 +0530

    Refactor regbank module for improved data handling

--- regbank.v
+++ regbank.v
@@ -1,22 +1,23 @@
-module regbank(data_in, addr, clk, clr, wr);     // this is just to write the data 
+module regbank(data_out1, data_out2, data_in, sr1, sr2, dr,clk,wr);     // this is just to write the data 
   parameter reg_size=8, bank_size=1024, add_size=10;
-  //output reg [reg_size-1:0] data_out;
-  input [reg_size-1:0] data_in;
-  input reg [add_size-1:0] addr;
-  input clk, clr;
+  output reg [reg_size-1:0] data_out;
+  input [reg_size-1:0] data_in1, data_in2;
+  input reg [add_size-1:0] sr1,sr2,dr;
+  input clk, wr;
   reg [reg_size-1:0] regist[bank_size-1:0];
-  int k;
+  //int k;
 
-  always @(posedge clk or clr)
+  always @(posedge clk)
     begin 
-      if(clr == 1) begin
-        for(k=0; k<bank_size-1; k= k+1)
-          regist[k] <= 0;
+      if(wr==0) regist[dr] <= data_in;
+      else if(wr == 0) begin 
+        data_out1 <= regist[sr1];
+        data_out2 <= regist[sr2];
       end
-      else if(wr) begin 
-        regist[addr] <= data_in;
+      else begin
+        data_out1 = z;
+        data_out2 = z;
       end
-      else regist[addr] = z;
     end 
 endmodule
 
